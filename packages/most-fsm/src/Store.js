495f0a51b20f623bbb75ed49150c9e89bafb356e
Pavel Tereschenko <me@stupidcat.wtf>
2018-06-07 00:50:21 +0300

    Add new methods to Store

--- packages/most-fsm/src/Store.js
+++ packages/most-fsm/src/Store.js
@@ -7,41 +7,58 @@ import { createSubject, createReplaySubject } from './Subject';
 import type { Subject } from './Subject';
 
 export type Case<S, P> = {|
-  state$: Stream<S>,
-  next: (payload: P) => void,
-  error: (error: Error) => void,
-  complete: (payload: void | P) => void,
+  +state$: Stream<S>,
+  next(payload: P): void,
+  error(error: Error): void,
+  complete(payload: void | P): void,
 |};
 
 export type Store<S> = {|
-  state$: Stream<S>,
-  case: <P>(handler: (state: S, payload: P) => Observable<S> | S) => Case<S, P>,
+  +state$: Stream<S>,
+  case<P>(handler: (state: S, payload: P) => Observable<S> | S): Case<S, P>,
+  onState(handler: (state: S) => Observable<S> | S): Case<S, void>,
+  onPayload<P>(handler: (payload: P) => Observable<S> | S): Case<S, P>,
+  just(): Case<S, S>,
+  always(value: S): Case<S, void>,
 |};
 
-export const createStore = <S>(initialState: S): Store<S> => {
+type CreateStore = <S>(initialState: S) => Store<S>;
+
+export const createStore: CreateStore = <S>(initialState: S): Store<S> => {
   const storeSubject: Subject<S> = createReplaySubject(initialState);
   const state$: Stream<S> = storeSubject.stream.multicast();
 
+  const initCase = <P>(
+    handler: (state: S, payload: P) => Observable<S> | S,
+  ): Case<S, P> => {
+    const { next, error, complete, stream }: Subject<P> = createSubject();
+
+    const case$: Stream<S> = stream
+      .sample(handler, state$, stream)
+      .chain(
+        (value: Observable<S> | S) =>
+          value && typeof value.subscribe == 'function'
+            ? from(((value: any): Observable<S>))
+            : of(((value: any): S)),
+      )
+      .multicast();
+
+    case$.subscribe(storeSubject);
+
+    return { next, error, complete, state$: case$ };
+  };
+
   return {
     state$,
-    case: <P>(
-      handler: (state: S, payload: P) => Observable<S> | S,
-    ): Case<S, P> => {
-      const { next, error, complete, stream }: Subject<P> = createSubject();
+    case: initCase,
 
-      const case$ = stream
-        .sample(handler, state$, stream)
-        .chain(
-          (value: Observable<S> | S) =>
-            value && typeof value.subscribe == 'function'
-              ? from(((value: any): Observable<S>))
-              : of(((value: any): S)),
-        )
-        .multicast();
+    onState: (handler): Case<S, void> => initCase(state => handler(state)),
 
-      case$.subscribe(storeSubject);
+    onPayload: <P>(handler: (payload: P) => Observable<S> | S): Case<S, P> =>
+      initCase((state, payload) => handler(payload)),
 
-      return { next, error, complete, state$: case$ };
-    },
+    just: (): Case<S, S> => initCase((_, payload) => payload),
+
+    always: (value): Case<S, void> => initCase(() => value),
   };
 };
