0c5226dd45acf5adaa6a1caffa330c5aa9649b6d
Pavel Tereschenko <me@stupidcat.wtf>
2018-07-02 19:28:59 +0300

    Fix babel config

---
+++ babel.config.js
@@ -0,0 +1,25 @@
+/* @flow */
+
+module.exports = {
+  plugins: ['@babel/plugin-proposal-class-properties'],
+  presets: [
+    '@babel/flow',
+    '@babel/react',
+    [
+      '@babel/env',
+      {
+        targets: {
+          node: 6,
+          browsers: [
+            'last 5 Chrome versions',
+            'last 5 Firefox versions',
+            'iOS >= 9',
+            'ie >= 10',
+          ],
+        },
+        modules: false,
+        useBuiltIns: 'usage',
+      },
+    ],
+  ],
+};
