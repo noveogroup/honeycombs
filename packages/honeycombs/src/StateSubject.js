a0633ac4837f399485f377fddbfd03b701a6c80b
Pavel Tereschenko <me@stupidcat.wtf>
2018-07-02 20:04:05 +0300

    Implement StateSubject

---
+++ packages/honeycombs/src/StateSubject.js
@@ -0,0 +1,33 @@
+/* @flow */
+
+import { Subscription, type ObserverInterface } from 'es-observable';
+
+import { Subject } from './Subject';
+import { SimpleStore } from './SimpleStore';
+
+export class StateSubject<S> extends Subject<S> {
+  #store /* : SimpleStore<S> */;
+
+  constructor(store: SimpleStore<S>) {
+    super();
+    this.#store = store;
+  }
+
+  subscribe(observer: (S => any) | ObserverInterface<S>): Subscription<S> {
+    const state = this.#store.getState();
+
+    if (typeof observer == 'function') observer(state);
+    else if (observer.next) observer.next(state);
+
+    return Subject.prototype.subscribe.call(this, observer);
+  }
+
+  /* ::
+  +subscribe: ((
+    next: (S) => any,
+    error?: (Error) => any,
+    complete?: () => any,
+  ) => Subscription<S>) &
+    ((observer: ObserverInterface<S>) => Subscription<S>);
+  */
+}
