30b9c5c4cf348f7814ccb01b2130b19a3dbda6da
Pavel Tereschenko <me@stupidcat.wtf>
2018-07-03 04:29:53 +0300

    Add Subject tests

---
+++ packages/honeycombs/src/Subject.test.js
@@ -0,0 +1,21 @@
+/* @flow */
+/* eslint import/no-extraneous-dependencies: off */
+
+import test from 'ava';
+
+import { reduce } from 'es-observable/src/reduce';
+
+import { Subject } from './Subject';
+
+test('Subject', async t => {
+  const subject = new Subject();
+
+  setTimeout(() => {
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+    subject.complete();
+  }, 0);
+
+  t.deepEqual(await reduce(subject), [1, 2, 3]);
+});
