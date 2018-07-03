76b5b1d129a1180f80a9995c14823ff4a8332a61
Pavel Tereschenko <me@stupidcat.wtf>
2018-07-03 04:28:58 +0300

    Implement reduce function

---
+++ packages/es-observable/src/reduce.js
@@ -0,0 +1,21 @@
+/* @flow */
+
+import type { ObservableInterface } from './types';
+
+export const reduce = <T>(
+  observable: ObservableInterface<T>,
+): Promise<$ReadOnlyArray<T>> =>
+  new Promise((resolve, reject) => {
+    const acc = [];
+    observable.subscribe({
+      next(value) {
+        acc.push(value);
+      },
+      error(error) {
+        reject(error);
+      },
+      complete() {
+        resolve(acc);
+      },
+    });
+  });
